655336305130d1574c8b1a601ea9c4284a6570af
quekhyg <quekhyg@gmail.com>
2022-06-26 17:01:25 +0800

    cicd attempt

--- a-working/training_pipeline.py
+++ a-working/training_pipeline.py
@@ -1,26 +1,31 @@
+import kfp.dsl as dsl
 @dsl.pipeline(
     name="training-pipeline",
     description="Training pipeline attempt",
     pipeline_root=PIPELINE_ROOT,
 )
 
-def pipeline(text: str = "hi there"):
-    hw_task = hello_world(text)
-    two_outputs_task = two_outputs(text)
+def training_pipeline(project_dict: dict = {
+    'PROJECT_ID' = 'mle-airbus-detection-smu',
+    'GCS_BUCKET' = 'mle_airbus_dataset',
+    'REGION' = 'asia-east1',
+    'TABLE_BQ' = 'placeholder'}):
+    
+    import_file = import_file_component(project_dict)
 
-consumer_task = consumer( # noqa: F841
-    hw_task.output,
-    two_outputs_task.outputs["output_one"],
-    two_outputs_task.outputs["output_two"],
-)
+    consumer_task = consumer( # noqa: F841
+        import_file.output,
+    )
+
 
 from kfp.v2 import compiler # noqa: F811
-compiler.Compiler().compile(pipeline_func=pipeline,
-package_path="intro_pipeline.json")
+
+compiler.Compiler().compile(pipeline_func=train_pipeline, package_path="training_pipeline.json")
 DISPLAY_NAME = "intro_" + TIMESTAMP
+
 job = aip.PipelineJob(
-display_name=DISPLAY_NAME,
-template_path="intro_pipeline.json",
-pipeline_root=PIPELINE_ROOT,
+    display_name=DISPLAY_NAME,
+    template_path="training_pipeline.json",
+    pipeline_root=PIPELINE_ROOT,
 )
 job.submit()
